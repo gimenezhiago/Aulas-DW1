4b7a452f94bc7e0b7860935b27469ad5fe3535c5
gimenezhiago <163415252+gimenezhiago@users.noreply.github.com>
2024-07-04 11:28:54 -0300

    juyjh

--- hgf.js
+++ hgf.js
@@ -1,6 +1,34 @@
-var pais = 'EUA'
-if (pais = 'Brasil'){
-    console.log('É brasileira')
+var idade = 12
+
+if (idade < 16) {
+    console.log(`n vota`)
+} else if (idade < 18 || idade > 65) {
+        console.log(`opcional`)
 } else {
-    console.log('é EUa')
+    console.log(`obrigatorio`)
+}
+
+var agora = new Date()
+var hora = agora.getHours()
+
+if (hora < 12) {
+    console.log(`bom dia`)
+} else if (hora <= 18){
+    console.log(`bom tarde`)
+} else {
+    console.log(`bom noite`)
+}
+
+var agora = new Date()
+var diaSem =  agora.getDay()
+switch(diaSem) {
+    case 0:
+        Domigno
+        break
+    case 1:
+        segunda
+        break
+    default:
+        erro
+        break
 }
