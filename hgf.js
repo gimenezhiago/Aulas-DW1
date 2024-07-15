17d4ab66ef638787b70ac0073c1f127416fedfef
gimenezhiago <163415252+gimenezhiago@users.noreply.github.com>
2024-07-15 13:11:14 -0300

    lkjhbgv

--- hgf.js
+++ hgf.js
@@ -1,34 +1,12 @@
-var idade = 12
+//var c = 1
 
-if (idade < 16) {
-    console.log(`n vota`)
-} else if (idade < 18 || idade > 65) {
-        console.log(`opcional`)
-} else {
-    console.log(`obrigatorio`)
-}
+//while(c < 7) {
+//console.log(`Passo ${c}`)
+//c++
+//}
 
-var agora = new Date()
-var hora = agora.getHours()
-
-if (hora < 12) {
-    console.log(`bom dia`)
-} else if (hora <= 18){
-    console.log(`bom tarde`)
-} else {
-    console.log(`bom noite`)
-}
-
-var agora = new Date()
-var diaSem =  agora.getDay()
-switch(diaSem) {
-    case 0:
-        Domigno
-        break
-    case 1:
-        segunda
-        break
-    default:
-        erro
-        break
-}
+var c = 1;
+do {
+  console.log(`passo ${c}`);
+  c++;
+} while (c <= 6);
