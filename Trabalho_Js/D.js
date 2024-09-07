91b452908d650610f9bfbca1f064c4a3b7123f61
Hiago Gimenez <163415252+gimenezhiago@users.noreply.github.com>
2024-09-07 13:35:12 -0300

    gffhjj

---
+++ Trabalho_Js/D.js
@@ -0,0 +1,34 @@
+<h1>Escolha um número de 1 até 8:</h1>
+let number = 8
+
+switch (date) {
+    case 0:
+
+    case 1:
+        console.log("1");
+      break;
+    case 2:
+        console.log("2");
+      break;
+    case 3:
+        console.log("3");
+      break;
+    case 4:
+        console.log("4");
+      break;
+    case 5:
+        console.log("5");
+      break;
+    case 6:
+        console.log("6");
+      break;
+    case 7:
+        console.log("7");
+    break;
+    case 8:
+        console.log("8");
+    break;
+    default:
+        console.log("Sem opção");
+      break;
+}
