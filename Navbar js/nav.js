d01b0a338b1ac669797d2a86f78ff8ba01f667b4
Hiago Gimenez <163415252+gimenezhiago@users.noreply.github.com>
2024-11-06 14:30:23 -0300

    hfgvjnk

--- Navbar js/nav.js
+++ Navbar js/nav.js
@@ -1,1 +1,7 @@
-let open  = document.querySelector()
+const navbar = document.querySelector(".navbar")
+const lista = document.querySelector("nav ul")
+
+navbar.addEventListener("click", () => {
+  lista.classList.toggle("show")
+  navbar.textContent = navbar.textContent === "☰" ? "✖" : "☰"
+});
